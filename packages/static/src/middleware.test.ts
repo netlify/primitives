6546e118c0ceb50385dac16c79c20f3f50d84384
Eduardo Bouças <mail@eduardoboucas.com>
2024-11-27 14:55:05 +0000

    chore: fix test

--- packages/static/src/middleware.test.ts
+++ packages/static/src/middleware.test.ts
@@ -1,6 +1,6 @@
 import { join } from 'node:path'
 
-import { Fixture } from '@netlify/dev'
+import { Fixture, Server } from '@netlify/dev'
 import { describe, expect, test } from 'vitest'
 
 import { withStatic } from './middleware.js'
@@ -26,17 +26,17 @@ describe('`withStatic` middleware', () => {
       directory: join(directory, "public")
     })
 
-    const next = async () => new Response('Fallback', { status: 418 })
+    const server = new Server().use(middleware).use(() => new Response('Fallback', { status: 418 }))
 
     const req1 = new Request('https://site.netlify/unknown')
-    const res1 = await middleware(req1, {}, next)
+    const res1 = await server.handleRequest(req1)
     expect(res1?.status).toBe(418)
     expect(await res1?.text()).toBe('Fallback')
     expect(res1?.headers.get("age")).toBeNull()
     expect(res1?.headers.get("cache-control")).toBeNull()
 
     const req2 = new Request('https://site.netlify')
-    const res2 = await middleware(req2, {}, next)
+    const res2 = await server.handleRequest(req2)
     expect(res2?.status).toBe(200)
     expect(await res2?.text()).toBe('Hello from the root')
     expect(res2?.headers.get("age")).toBe("0")
@@ -44,7 +44,7 @@ describe('`withStatic` middleware', () => {
     expect(res2?.headers.get("content-type")).toBe("text/html; charset=utf-8")
 
     const req3 = new Request('https://site.netlify/')
-    const res3 = await middleware(req3, {}, next)
+    const res3 = await server.handleRequest(req3)
     expect(res3?.status).toBe(200)
     expect(await res3?.text()).toBe('Hello from the root')
     expect(res3?.headers.get("age")).toBe("0")
@@ -52,7 +52,7 @@ describe('`withStatic` middleware', () => {
     expect(res3?.headers.get("content-type")).toBe("text/html; charset=utf-8")
 
     const req4 = new Request('https://site.netlify/store')
-    const res4 = await middleware(req4, {}, next)
+    const res4 = await server.handleRequest(req4)
     expect(res4?.status).toBe(200)
     expect(await res4?.text()).toBe('Hello from the store')
     expect(res4?.headers.get("age")).toBe("0")
@@ -60,7 +60,7 @@ describe('`withStatic` middleware', () => {
     expect(res4?.headers.get("content-type")).toBe("text/html; charset=utf-8")
 
     const req5 = new Request('https://site.netlify/store/index.html')
-    const res5 = await middleware(req5, {}, next)
+    const res5 = await server.handleRequest(req5)
     expect(res5?.status).toBe(200)
     expect(await res5?.text()).toBe('Hello from the store')
     expect(res5?.headers.get("age")).toBe("0")
