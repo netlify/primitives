b02e85b40d30c2b147319d431366cbb18556d574
Eduardo Bouças <mail@eduardoboucas.com>
2025-02-20 10:24:14 +0000

    refactor: revert changes

--- packages/cache/src/fetchwithcache.ts
+++ packages/cache/src/fetchwithcache.ts
@@ -43,7 +43,7 @@ const isRequestInit = (input: any): input is RequestInit => {
   }
 
   for (const property of requestInitOptions) {
-    if (Object.hasOwn(input, property)) {
+    if (property in input) {
       return true
     }
   }
