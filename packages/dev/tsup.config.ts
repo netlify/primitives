695514c67e244c32b5f6eb56dfafb268cab728ff
Eduardo Bouças <mail@eduardoboucas.com>
2025-04-14 18:07:35 +0100

    chore: use tsup

---
+++ packages/dev/tsup.config.ts
@@ -0,0 +1,17 @@
+import { argv } from 'node:process'
+
+import { defineConfig } from 'tsup'
+
+export default defineConfig([
+  {
+    clean: true,
+    entry: ['src/main.ts'],
+    outDir: 'dist',
+    format: ['cjs', 'esm'],
+    dts: true,
+    splitting: false,
+    watch: argv.includes('--watch'),
+    platform: 'node',
+    bundle: true,
+  },
+])
