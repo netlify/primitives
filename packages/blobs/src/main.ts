ba1f6bcb513fa1a6312986f626626983b554487a
Eduardo Bouças <mail@eduardoboucas.com>
2025-04-14 23:30:04 +0100

    feat: update blobs

--- packages/blobs/src/main.ts
+++ packages/blobs/src/main.ts
@@ -1,4 +1,4 @@
-export { EnvironmentContext, setEnvironmentContext } from './environment.ts'
+export { setEnvironmentContext } from './environment.ts'
 export { connectLambda } from './lambda_compat.ts'
 export { getDeployStore, getStore, type GetStoreOptions, type GetDeployStoreOptions } from './store_factory.ts'
 export { listStores } from './store_list.ts'
