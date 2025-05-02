b23c607ed3aa5e76279efa773b8c6c4d0dee972c
Eduardo Bouças <mail@eduardoboucas.com>
2025-05-02 09:52:47 +0100

    feat!: add `start` entry point (#154)
    BREAKING CHANGE: The `BlobsServer` class now only exports the `start()` and `stop()` methods. This class is not part of the Netlify Blobs client, and it's mostly used internally by Netlify tooling and by some users for integration tests — if you're just using the methods listed in https://docs.netlify.com/blobs/overview/#api-reference, this change does not apply to you and you can safely upgrade.

---
+++ packages/dev/src/lib/runtime.ts
@@ -0,0 +1,78 @@
+import path from 'node:path'
+import process from 'node:process'
+
+import { BlobsServer } from '@netlify/blobs/server'
+import { type EnvironmentVariables, startRuntime } from '@netlify/runtime'
+
+interface GetRuntimeOptions {
+  blobs: boolean
+  blobsToken?: string
+  deployID: string
+  projectRoot: string
+  siteID: string
+}
+
+const restoreEnvironment = (snapshot: Record<string, string | undefined>) => {
+  for (const key in snapshot) {
+    if (snapshot[key] === undefined) {
+      delete process.env[key]
+    } else {
+      process.env[key] = snapshot[key]
+    }
+  }
+}
+
+export const getRuntime = async ({ blobs, deployID, projectRoot, siteID }: GetRuntimeOptions) => {
+  const blobsToken = Math.random().toString().slice(2)
+  const blobsServer = blobs
+    ? new BlobsServer({
+        directory: path.join(projectRoot, '.netlify', 'blobs-serve'),
+        token: blobsToken,
+      })
+    : null
+  const blobsServerDetails = await blobsServer?.start()
+
+  const envSnapshot: Record<string, string | undefined> = {}
+  const env: EnvironmentVariables = {
+    delete: (key: string) => {
+      envSnapshot[key] = envSnapshot[key] || process.env[key]
+
+      delete process.env[key]
+    },
+    get: (key: string) => process.env[key],
+    has: (key: string) => Boolean(process.env[key]),
+    set: (key: string, value: string) => {
+      envSnapshot[key] = envSnapshot[key] || process.env[key]
+
+      process.env[key] = value
+    },
+    toObject: () => process.env as Record<string, string>,
+  }
+
+  startRuntime({
+    blobs: blobsServerDetails
+      ? {
+          edgeURL: `http://localhost:${blobsServerDetails.port}`,
+          uncachedEdgeURL: `http://localhost:${blobsServerDetails.port}`,
+          primaryRegion: 'us-east-2',
+          token: blobsToken,
+        }
+      : undefined,
+    cache: {
+      getCacheAPIContext: () => null,
+      purgeToken: '',
+    },
+    deployID,
+    env,
+    getRequestContext: () => null,
+    siteID,
+  })
+
+  return {
+    stop: async () => {
+      restoreEnvironment(envSnapshot)
+
+      await blobsServer?.stop()
+    },
+  }
+}
--- packages/dev/src/main.ts
+++ packages/dev/src/main.ts
@@ -7,43 +7,73 @@ import { StaticHandler } from '@netlify/static'
 
 import { getConfig } from './lib/config.js'
 import { isDirectory } from './lib/fs.js'
+import { getRuntime } from './lib/runtime.js'
+
+interface Features {
+  blobs?: boolean
+  functions?: boolean
+  redirects?: boolean
+  static?: boolean
+}
 
 interface HandleOptions {
+  features?: Features
   projectRoot?: string
+}
+
+const defaultFeatures: Features = {
+  blobs: true,
+  functions: true,
+  redirects: true,
+  static: true,
 }
 
 const notFoundHandler = async () => new Response('Not found', { status: 404 })
 
 export const handle = async (request: Request, options?: HandleOptions) => {
-  const { projectRoot = process.cwd() } = options ?? {}
+  const { features = defaultFeatures, projectRoot = process.cwd() } = options ?? {}
   const { config, siteID } = (await getConfig({ projectRoot })) ?? {}
 
   // Functions
   const userFunctionsPath = config?.config.functionsDirectory ?? path.join(projectRoot, 'netlify/functions')
   const userFunctionsPathExists = await isDirectory(userFunctionsPath)
-  const functions = new FunctionsHandler({
-    config,
-    destPath: path.join(projectRoot, '.netlify', 'functions-serve'),
-    projectRoot,
-    settings: {},
-    siteId: siteID,
-    timeouts: {},
-    userFunctionsPath: userFunctionsPathExists ? userFunctionsPath : undefined,
-  })
+  const functions = features.functions
+    ? new FunctionsHandler({
+        config,
+        destPath: path.join(projectRoot, '.netlify', 'functions-serve'),
+        projectRoot,
+        settings: {},
+        siteId: siteID,
+        timeouts: {},
+        userFunctionsPath: userFunctionsPathExists ? userFunctionsPath : undefined,
+      })
+    : null
 
   // Redirects
-  const redirects = new RedirectsHandler({
-    configPath: config?.configPath,
-    configRedirects: config?.config.redirects,
-    jwtRoleClaim: '',
-    jwtSecret: '',
-    notFoundHandler,
-    projectDir: projectRoot,
-  })
+  const redirects = features.redirects
+    ? new RedirectsHandler({
+        configPath: config?.configPath,
+        configRedirects: config?.config.redirects,
+        jwtRoleClaim: '',
+        jwtSecret: '',
+        notFoundHandler,
+        projectDir: projectRoot,
+      })
+    : null
 
   // Static files
-  const staticFiles = new StaticHandler({
-    directory: config?.config.build.publish ?? projectRoot,
+  const staticFiles = features.static
+    ? new StaticHandler({
+        directory: config?.config.build.publish ?? projectRoot,
+      })
+    : null
+
+  // Runtime
+  const runtime = await getRuntime({
+    blobs: Boolean(features.blobs),
+    deployID: '0',
+    projectRoot,
+    siteID: siteID ?? '0',
   })
 
   // Try to match the request against the different steps in our request chain.
@@ -51,12 +81,12 @@ export const handle = async (request: Request, options?: HandleOptions) => {
   // https://docs.netlify.com/platform/request-chain/
 
   // 1. Check if the request matches a function.
-  const functionMatch = await functions.match(request)
+  const functionMatch = await functions?.match(request)
   if (functionMatch) {
     // If the function prefers static files, check if there is a static match
     // and, if so, return that
     if (functionMatch.preferStatic) {
-      const staticMatch = await staticFiles.match(request)
+      const staticMatch = await staticFiles?.match(request)
 
       if (staticMatch) {
         return staticMatch.handle()
@@ -68,18 +98,18 @@ export const handle = async (request: Request, options?: HandleOptions) => {
   }
 
   // 2. Check if the request matches a redirect rule.
-  const redirectMatch = await redirects.match(request)
+  const redirectMatch = await redirects?.match(request)
   if (redirectMatch) {
     // If the redirect rule matches a function, we'll serve it. The exception
     // is if the function prefers static files, which in this case means that
     // we'll follow the redirect rule.
-    const functionMatch = await functions.match(new Request(redirectMatch.target))
+    const functionMatch = await functions?.match(new Request(redirectMatch.target))
     if (functionMatch && !functionMatch.preferStatic) {
       return functionMatch.handle(request)
     }
 
-    const response = await redirects.handle(request, redirectMatch, async (maybeStaticFile: Request) => {
-      const staticMatch = await staticFiles.match(maybeStaticFile)
+    const response = await redirects?.handle(request, redirectMatch, async (maybeStaticFile: Request) => {
+      const staticMatch = await staticFiles?.match(maybeStaticFile)
 
       return staticMatch?.handle
     })
@@ -89,8 +119,31 @@ export const handle = async (request: Request, options?: HandleOptions) => {
   }
 
   // 3. Check if the request matches a static file.
-  const staticMatch = await staticFiles.match(request)
+  const staticMatch = await staticFiles?.match(request)
   if (staticMatch) {
     return staticMatch.handle()
+  }
+
+  await runtime.stop()
+}
+
+interface StartOptions {
+  projectRoot?: string
+}
+
+export const start = async (options?: StartOptions) => {
+  const { projectRoot = process.cwd() } = options ?? {}
+  const { siteID } = (await getConfig({ projectRoot })) ?? {}
+  const runtime = await getRuntime({
+    blobs: true,
+    deployID: '0',
+    projectRoot,
+    siteID: siteID ?? '0',
+  })
+
+  return {
+    stop: async () => {
+      await runtime.stop()
+    },
   }
 }
