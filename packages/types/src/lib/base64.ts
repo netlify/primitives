def1e19a7a3079a1c019adf811bf2c154e76ed48
Eduardo Bouças <mail@eduardoboucas.com>
2025-05-07 15:03:52 +0100

    chore: add README

--- packages/types/src/lib/base64.ts
+++
@@ -1,6 +0,0 @@
-
-// eslint-disable-next-line n/prefer-global/buffer
-export const base64Decode =  globalThis.Buffer ? (input: string) => Buffer.from(input, 'base64').toString() : (input: string) => atob(input)
-
-// eslint-disable-next-line n/prefer-global/buffer
-export const base64Encode = globalThis.Buffer ? (input: string) => Buffer.from(input).toString('base64') : (input: string) => btoa(input)
