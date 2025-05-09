4ed760bf7e00e5e8650dd0f56714175f6dbc91bb
Chris Shaw <me@cshaw.xyz>
2025-05-10 00:42:38 +1000

    fix: pass through server port when configured

--- packages/blobs/src/server.ts
+++ packages/blobs/src/server.ts
@@ -73,6 +73,7 @@ export class BlobsServer {
 
   constructor({ debug, directory, logger, onRequest, port, token }: BlobsServerOptions) {
     this.address = ''
+    this.port = port
     this.debug = debug === true
     this.directory = directory
     this.logger = logger ?? console.log
@@ -534,7 +535,7 @@ export class BlobsServer {
     await fs.mkdir(this.directory, { recursive: true })
 
     const server = new HTTPServer((req) => this.handleRequest(req))
-    const address = await server.start()
+    const address = await server.start(this.port ?? 0)
     const port = Number.parseInt(new URL(address).port)
 
     this.address = address
