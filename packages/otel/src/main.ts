4056cc4d1631ac0b7f94b7aac578a0e7b48defb6
Jake Champion <jake.champion@netlify.com>
2025-05-12 13:55:08 +0100

    feat(otel): initialize OpenTelemetry package with tracer provider and span exporter (#188)

---
+++ packages/otel/src/main.ts
@@ -0,0 +1,15 @@
+import { type SugaredTracer } from '@opentelemetry/api/experimental';
+import { GET_TRACER, SHUTDOWN_TRACERS } from './constants.js';
+
+type GlobalThisExtended = typeof globalThis & {
+    [GET_TRACER]?: (name?: string, version?: string) => SugaredTracer | undefined;
+    [SHUTDOWN_TRACERS]?: () => void;
+}
+
+export const getTracer = async (name?: string, version?: string): Promise<SugaredTracer | undefined> => {
+    return (globalThis as GlobalThisExtended)[GET_TRACER]?.(name, version);
+}
+
+export const shutdownTracers = async (): Promise<void> => {
+    return (globalThis as GlobalThisExtended)[SHUTDOWN_TRACERS]?.();
+}
