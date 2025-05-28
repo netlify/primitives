d44fc4a6cf7d18bd5652215101006ef532ff91a2
Eduardo Bouças <mail@eduardoboucas.com>
2025-05-28 13:56:54 +0100

    chore: remove unused types

--- packages/edge-functions/dev/shared/types.ts
+++ packages/edge-functions/dev/shared/types.ts
@@ -1,13 +1,6 @@
 import type { Config } from '../../src/lib/config.ts'
 
-export interface AvailableFunction {
-  config?: Config
-  path: string
-}
-
 export type EdgeFunction = { config?: Config; default: (req: Request, context: object) => Promise<Response> }
-
-export interface FunctionMapEntry {}
 
 export interface RunOptions {
   bootstrapURL: string
