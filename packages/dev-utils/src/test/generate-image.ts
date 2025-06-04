6986b42f0765cf4c33461906887cd19eb2c82e2e
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-06-04 13:35:55 +0200

    chore: add jsdoc for generateImage

--- packages/dev-utils/src/test/generate-image.ts
+++ packages/dev-utils/src/test/generate-image.ts
@@ -1,5 +1,8 @@
 import { generateImage as generateImageCallback } from 'js-image-generator'
 
+/**
+ * Returns Buffer of a generated random noise jpeg image with the specified width and height.
+ */
 export async function generateImage(width: number, height: number): Promise<Buffer> {
   return new Promise<Buffer>((resolve, reject) => {
     generateImageCallback(width, height, 80, (error, image) => {
