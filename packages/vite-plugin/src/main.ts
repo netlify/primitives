214ac26f0da3e598b29e8ee7c8c9434de4468703
Matt Kane <m@mk.gg>
2025-08-28 18:22:55 +0100

    fix(vite-plugin): pass environmentVariables option to NetlifyDev (#442)
    * fix(vite-plugin): pass environmentVariables option to NetlifyDev

--- packages/vite-plugin/src/main.ts
+++ packages/vite-plugin/src/main.ts
@@ -30,11 +30,21 @@ export default function netlify(options: NetlifyPluginOptions = {}): any {
         return
       }
       const logger = createLoggerFromViteLogger(viteDevServer.config.logger)
-      const { blobs, edgeFunctions, functions, images, middleware = true, redirects, staticFiles } = options
+      const {
+        blobs,
+        edgeFunctions,
+        environmentVariables,
+        functions,
+        images,
+        middleware = true,
+        redirects,
+        staticFiles,
+      } = options
 
       const netlifyDev = new NetlifyDev({
         blobs,
         edgeFunctions,
+        environmentVariables,
         functions,
         images,
         logger,
