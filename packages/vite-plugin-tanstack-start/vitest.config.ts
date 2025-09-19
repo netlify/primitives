7393ea17dc06cd0735d7011407b3c8c5d34ee79b
Philippe Serhal <philippe.serhal@gmail.com>
2025-09-19 07:02:01 -0400

    chore: try a longer hook timeout

--- packages/vite-plugin-tanstack-start/vitest.config.ts
+++ packages/vite-plugin-tanstack-start/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     testTimeout: 15_000,
-    hookTimeout: 30_000,
+    hookTimeout: 90_000,
     env: {
       // See https://github.com/webdiscus/ansis/?tab=readme-ov-file#disable-colors-in-tests
       NO_COLOR: 'true',
