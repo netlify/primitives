b81b724411750b682a08ea7b28383f7a79f3e8e4
Philippe Serhal <philippe.serhal@gmail.com>
2025-09-21 15:56:02 -0400

    chore: try even higher Windows e2e timeout

--- packages/vite-plugin-tanstack-start/vitest.config.ts
+++ packages/vite-plugin-tanstack-start/vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     testTimeout: 15_000,
     // e2e fixture install & deploy time is highly variable on GitHub's CI infra, especially Windows :(
-    hookTimeout: 5 * 60_000,
+    hookTimeout: (process.platform === 'win32' ? 10 : 5) * 60_000,
     env: {
       // See https://github.com/webdiscus/ansis/?tab=readme-ov-file#disable-colors-in-tests
       NO_COLOR: 'true',
