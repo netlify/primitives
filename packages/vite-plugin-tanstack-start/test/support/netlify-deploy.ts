fd8b2cc9012801c190f332089e9f4322fad95a45
Philippe Serhal <philippe.serhal@netlify.com>
2025-09-22 07:03:32 -0400

    feat: add `@netlify/vite-plugin-tanstack-start` (#455)
    * feat: add `@netlify/vite-plugin-tanstack-start`
    
    This adds a framework-agnostic Vite build plugin that prepares a Vite app for deployment to Netlify. It expects that the
    Vite app has an `ssr` environment (and no other server environments to deploy) that has exactly one bundle entry and
    that this entry is a server request entry point, a module with a default export containing a Web Fetch handler under the
    `fetch` property. TanStack Start meets all these conditions.
    
    This build plugin can be opted into via the intentionally private, undocumented `build.enabled` option, passed into
    `@netlify/vite-plugin`.
    
    This also adds a new `@netlify/vite-plugin-tanstack-start` which just wraps `@netlify/vite-plugin` with this enabled.
    
    At the moment, this only supports TanStack Start `alpha`, but this will be released imminently (presumably v.1.132 or
    so). As such, it inherits its requirements:
    - Vite 7+
    - Node.js 22.12.0+
    
    * chore: try a longer hook timeout
    
    * refactor: remove unnecessary internal dep
    
    * fix(docs): fix incorrect options in readme
    
    * fix(types): add clearer private callout
    
    * chore: fix potential e2e deploy helper issue
    
    * ci: try even higher timeout for Windows
    
    * chore: try npm for e2e fixture install
    
    pnpm seems to be problematic on Windows CI...
    
    * fix: revert vite plugin type fix for now
    
    We'll do this in a separate PR to be safe.
    
    * chore: actually use npm correctly in deploy helper.
    
    follow-up to d117f25
    
    You can't use `overrides` for a direct dependency unless the values are identical...
    
    * fix: handle duplicate instances of our plugin
    
    Because we now have framework-specific plugins that load our generic plugin, we should make sure
    nothing bad happens if a user also includes our generic plugin directly.
    
    I went down a few different paths here and this ended up seeming like the best approach: just warn
    when we detect multiple instances in dev. Build is tricky because by design our plugin will be
    instantiated once per environment (e.g. client + ssr). And it ends up generally not being a problem
    if it's duplicated in build anyway, just dev.
    
    * chore: better error logging for failed e2e deploy
    
    * chore: fix e2e deploy bug
    
    * chore: actually bump e2e timeout
    
    math is hard
    
    * chore: improve e2e deploy logging
    
    * chore: try even higher Windows e2e timeout
    
    * test: skip tanstack plugin Windows tests for now
    
    also fix technically incorrect node version check

---
+++ packages/vite-plugin-tanstack-start/test/support/netlify-deploy.ts
@@ -0,0 +1,89 @@
+import { exec as originalExec } from 'node:child_process'
+import { writeFile, readFile } from 'node:fs/promises'
+import { join } from 'node:path'
+import { URL } from 'node:url'
+import { promisify } from 'node:util'
+
+import normalizePackageData, { type Package } from 'normalize-package-data'
+
+const exec = promisify(originalExec)
+
+// https://app.netlify.com/sites/tanstack-start-e2e-tests
+const SITE_ID = process.env.NETLIFY_SITE_ID ?? 'cd4e45ce-895c-432b-af6d-61e10ad1d125'
+
+export interface Deploy {
+  deployId: string
+  url: string
+  logs: string
+}
+
+// TODO(serhalp): List all monorepo packages dynamically? It gets super confusing when you
+// have changes to some transitive package and it fails in a weird way...
+const packages = [
+  { name: '@netlify/dev-utils', dirName: 'dev-utils' },
+  { name: '@netlify/vite-plugin', dirName: 'vite-plugin' },
+  { name: '@netlify/vite-plugin-tanstack-start', dirName: 'vite-plugin-tanstack-start' },
+]
+
+/**
+ * Inject the current revision of this repo's packages into the fixture.
+ *
+ * We can't use a simpler approach like a monorepo workspace or `npm link` because the fixture site
+ * needs to be self-contained to be deployable to Netlify (i.e. it can't have symlinks).
+ */
+const prepareDeps = async (cwd: string, packagesAbsoluteDir: string): Promise<void> => {
+  const packageJson = JSON.parse(await readFile(`${cwd}/package.json`, 'utf-8')) as Package & {
+    overrides?: Record<string, string>
+  }
+  normalizePackageData(packageJson)
+  packageJson.overrides ??= {}
+  const { dependencies = {}, devDependencies = {} } = packageJson
+  for (const pkg of packages) {
+    const isDep = pkg.name in dependencies
+    const isDevDep = pkg.name in devDependencies
+    console.log(`💉 Injecting local ${pkg.name} ${isDevDep ? 'dev ' : ''}dependency`)
+    const { stdout } = await exec(`npm pack --json --ignore-scripts --pack-destination ${cwd}`, {
+      cwd: join(packagesAbsoluteDir, pkg.dirName),
+    })
+    const [{ filename }] = JSON.parse(stdout) as { filename: string }[]
+    if (isDep) {
+      dependencies[pkg.name] = `file:${filename}`
+    }
+    if (isDevDep) {
+      devDependencies[pkg.name] = `file:${filename}`
+    }
+    // Ensure that even a transitive dependency on this package is overridden.
+    packageJson.overrides[pkg.name] = `file:${filename}`
+  }
+  await writeFile(`${cwd}/package.json`, JSON.stringify(packageJson, null, 2))
+  console.log('📦 Installing dependencies...')
+  await exec('npm install --no-package-lock', { cwd })
+  console.log('📦 Installed dependencies')
+}
+
+export const deploySite = async (projectDir: string): Promise<Deploy> => {
+  await prepareDeps(projectDir, join(import.meta.dirname, '../../../'))
+
+  console.log(`🚀 Building and deploying site...`)
+  try {
+    // Ideally `netlify-cli` should be installed as a dev dep, but since it in turn
+    // depends on some packages in this monorepo, this runs into some issues (despite no actual
+    // circular dependencies). This should be easy to avoid but npm's workspaces feature is
+    // too basic.
+    const cmd = `npx -y netlify deploy --json --site ${SITE_ID}`
+    const { stdout, stderr } = await exec(cmd, { cwd: projectDir })
+    await writeFile(join(projectDir, '__deploy.stdout.log'), stdout, { encoding: 'utf-8' })
+    await writeFile(join(projectDir, '__deploy.stderr.log'), stderr, { encoding: 'utf-8' })
+    // NOTE: `--json` is needed because otherwise the pretty box may wrap the URL over 2+ lines
+    const [url] = new RegExp(/https:.+\.netlify\.app/gm).exec(stdout) ?? []
+    if (!url) {
+      throw new Error('Could not extract the URL from the build logs')
+    }
+    console.log(`🌍 Deployed site is live: ${url}`)
+    const [deployId] = new URL(url).host.split('--')
+    return { url, deployId, logs: `${stdout}\n${stderr}` }
+  } catch (err) {
+    console.error('❌ Deployment failed:', err)
+    throw err
+  }
+}
