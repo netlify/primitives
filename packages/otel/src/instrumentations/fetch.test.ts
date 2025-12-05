5632e54808de5cebd2d1a762019c1d41e245237e
Mateusz Bocian <mrstork@users.noreply.github.com>
2025-12-05 13:27:02 -0500

    test: no longer patching fetch

--- packages/otel/src/instrumentations/fetch.test.ts
+++ packages/otel/src/instrumentations/fetch.test.ts
@@ -1,8 +1,4 @@
-import { http, HttpResponse } from 'msw'
-import { setupServer } from 'msw/node'
-import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it, test } from 'vitest'
-import { createTracerProvider } from '../bootstrap/main.ts'
-import { shutdownTracers } from '../main.ts'
+import { describe, expect, test } from 'vitest'
 import { FetchInstrumentation } from './fetch.ts'
 
 describe('header exclusion', () => {
@@ -66,79 +62,5 @@ describe('header exclusion', () => {
     expect(attributes['http.request.header.authorization']).toBeTypeOf('string')
     expect(attributes['http.request.header.a']).toBeTypeOf('string')
     expect(attributes['http.request.header.b']).toBeTypeOf('string')
-  })
-})
-
-describe('patched fetch', () => {
-  const server = setupServer(
-    http.get('http://localhost:3000/ok', () => HttpResponse.json({ message: 'ok' })),
-    http.post('http://localhost:3000/ok', () => HttpResponse.json({ message: 'ok' })),
-  )
-
-  beforeAll(() => {
-    server.listen({ onUnhandledRequest: 'error' })
-  })
-
-  beforeEach(() => {
-    createTracerProvider({
-      serviceName: 'test-service',
-      serviceVersion: '1.0.0',
-      deploymentEnvironment: 'test',
-      siteUrl: 'https://example.com',
-      siteId: '12345',
-      siteName: 'example',
-      instrumentations: [new FetchInstrumentation()],
-    })
-  })
-
-  afterEach(async () => {
-    server.resetHandlers()
-    await shutdownTracers()
-  })
-
-  afterAll(() => {
-    server.close()
-  })
-
-  it('can GET url', async () => {
-    createTracerProvider({
-      serviceName: 'test-service',
-      serviceVersion: '1.0.0',
-      deploymentEnvironment: 'test',
-      siteUrl: 'https://example.com',
-      siteId: '12345',
-      siteName: 'example',
-      instrumentations: [new FetchInstrumentation()],
-    })
-
-    await expect(fetch('http://localhost:3000/ok').then((r) => r.json())).resolves.toEqual({ message: 'ok' })
-  })
-
-  it('can POST url', async () => {
-    await expect(
-      fetch('http://localhost:3000/ok', {
-        method: 'POST',
-        body: JSON.stringify({ hello: 'rabbit' }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }).then((r) => r.json()),
-    ).resolves.toEqual({ message: 'ok' })
-  })
-
-  it('can GET request', async () => {
-    const req = new Request('http://localhost:3000/ok')
-    await expect(fetch(req).then((r) => r.json())).resolves.toEqual({ message: 'ok' })
-  })
-
-  it('can POST request', async () => {
-    const req = new Request('http://localhost:3000/ok', {
-      method: 'POST',
-      body: JSON.stringify({ hello: 'rabbit' }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    await expect(fetch(req).then((r) => r.json())).resolves.toEqual({ message: 'ok' })
   })
 })
