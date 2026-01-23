c1ae30f2745f0a73e26e83334695e205a04ab47d
Philippe Serhal <philippe.serhal@netlify.com>
2026-01-23 08:01:02 -0500

    test: upgrade TanStack Start fixture to latest RC (#574)
    * test: upgrade TanStack Start fixture to latest RC
    
    * test: fix tanstack e2e fixture
    
    This issue seems to be occurring: https://github.com/TanStack/router/issues/4959.
    
    Applying the teeny fix from this PR https://github.com/TanStack/router/pull/4606 fixes it.

---
+++ packages/vite-plugin-tanstack-start/test/fixtures/start-basic-rc/vite.config.ts
@@ -0,0 +1,23 @@
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tailwindcss(),
+    tsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tanstackStart({
+      srcDirectory: "src",
+    }),
+    viteReact(),
+    netlify(),
+  ],
+});
