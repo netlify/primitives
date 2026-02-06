84ddb4fdabf49479b53ae9b01cee9dcd36043ab2
Eduardo Bouças <mail@eduardoboucas.com>
2026-02-06 12:09:11 +0000

    chore: increase timeout

--- packages/dev/vitest.config.ts
+++ packages/dev/vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     // Some of this package's tests are slow on Windows
-    testTimeout: 10_000,
+    testTimeout: 20_000,
   },
 })
