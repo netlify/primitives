8b9bfb9e3de5491da9db85cba8e7efbf9eb960a2
Eduardo Bouças <mail@eduardoboucas.com>
2026-03-19 10:45:11 +0000

    feat: add event handler types (#630)
    * feat: add event handler types
    
    * chore: format

--- packages/types/src/lib/events/identity.ts
+++ packages/types/src/lib/events/identity.ts
@@ -19,6 +19,24 @@ export interface User {
   appMetadata?: Record<string, unknown>
 }
 
-export interface IdentityEvent {
+interface IdentityEvent {
   user: User
 }
+
+interface IdentityHandlerResult {
+  user: User
+}
+
+export type UserLoginEvent = IdentityEvent
+export type UserSignupEvent = IdentityEvent
+export type UserValidateEvent = IdentityEvent
+export type UserModifiedEvent = IdentityEvent
+export type UserDeletedEvent = IdentityEvent
+
+type MaybePromise<T> = T | Promise<T>
+
+export type UserLoginHandler = (event: UserLoginEvent) => MaybePromise<IdentityHandlerResult | undefined>
+export type UserSignupHandler = (event: UserSignupEvent) => MaybePromise<IdentityHandlerResult | undefined>
+export type UserValidateHandler = (event: UserValidateEvent) => MaybePromise<IdentityHandlerResult | undefined>
+export type UserModifiedHandler = (event: UserModifiedEvent) => MaybePromise<IdentityHandlerResult | undefined>
+export type UserDeletedHandler = (event: UserDeletedEvent) => MaybePromise<undefined>
--- packages/types/src/lib/events/submission.ts
+++ packages/types/src/lib/events/submission.ts
@@ -1,3 +1,5 @@
-export interface FormSubmissionEvent {
+export interface FormSubmittedEvent {
   data: Record<string, string>
 }
+
+export type FormSubmittedHandler = (event: FormSubmittedEvent) => void | Promise<void>
