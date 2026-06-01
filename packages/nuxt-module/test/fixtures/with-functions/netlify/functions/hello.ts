0eb84931cddc2dd8fc7cb5c7ad5fdf1e74222159
Hrishikesh Kokate <hrishi5200@gmail.com>
2026-06-01 23:41:47 +0530

    remove adapters

--- packages/nuxt-module/test/fixtures/with-functions/netlify/functions/hello.ts
+++
@@ -1,7 +0,0 @@
-export default async function handler() {
-  return new Response(`Hello from ${process.env.DEPLOY_URL}`)
-}
-
-export const config = {
-  path: '/hello',
-}
--- packages/vite-plugin-tanstack-start/test/e2e/dev.test.ts
+++
@@ -1,5 +0,0 @@
-import { describe } from 'vitest'
-
-// TODO(serhalp): I had to cut my losses after spending a whole day trying to make this work.
-// Try again with a clear head later :/
-describe.todo('development')
