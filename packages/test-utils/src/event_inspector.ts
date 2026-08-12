a72b67f8852fc490d863a685903948fcc2709f0f
Philippe Serhal <philippe.serhal@netlify.com>
2026-08-12 08:43:54 -0400

    build(deps): avoid internal circular dependency

--- packages/test-utils/src/event_inspector.ts
+++ packages/test-utils/src/event_inspector.ts
@@ -1,8 +1,10 @@
 import { EventEmitter } from 'node:events'
 
-import type { DevEvent } from '@netlify/dev-utils'
-
 const DEFAULT_TIMEOUT = 5000
+
+interface InspectableEvent {
+  name: string
+}
 
 interface EventInspectorOptions {
   debug?: boolean
@@ -10,7 +12,7 @@ interface EventInspectorOptions {
 
 export class EventInspector extends EventEmitter {
   debug: boolean
-  events: DevEvent[]
+  events: InspectableEvent[]
 
   constructor({ debug }: EventInspectorOptions = {}) {
     super()
@@ -19,19 +21,19 @@ export class EventInspector extends EventEmitter {
     this.events = []
   }
 
-  handleEvent(event: DevEvent) {
+  handleEvent(event: InspectableEvent) {
     this.events.push(event)
 
     this.emit('eventReceived', event)
   }
 
-  waitFor(filter: (event: DevEvent) => boolean, timeoutMs = DEFAULT_TIMEOUT) {
+  waitFor(filter: (event: InspectableEvent) => boolean, timeoutMs = DEFAULT_TIMEOUT) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         reject(new Error(`\`waitFor\` timed out after ${timeoutMs} ms`))
       }, timeoutMs)
 
-      this.on('eventReceived', (event: DevEvent) => {
+      this.on('eventReceived', (event: InspectableEvent) => {
         if (this.debug) {
           console.log('[EventInspector] Event received:', event)
         }
